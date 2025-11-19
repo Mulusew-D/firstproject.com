09eddd2ead98e9279592c2350dc08dabfe55fd28
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-20 02:48:30 +0300

    Update Login.jsx

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -10,62 +10,69 @@ const Login = () => {
   const [confirmPassword, setConfirmPassword] = useState("");
 
   const { isAuthenticated, setIsAuthenticated } = useContext(Context);
-  const navigate = useNavigate();
+
+  const navigateTo = useNavigate();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(
-        "https://mediserve-backend.onrender.com/api/v1/user/login",
-        { email, password, confirmPassword, role: "Admin" },
-        {
-          withCredentials: true,
-          headers: { "Content-Type": "application/json" },
-        }
-      );
-
-      toast.success(res.data.message);
-      setIsAuthenticated(true);
-      navigate("/"); // redirect to dashboard
-      setEmail("");
-      setPassword("");
-      setConfirmPassword("");
+      await axios
+        .post(
+          "https://mediserve-backend.onrender.com/api/v1/user/login",
+          { email, password, confirmPassword, role: "Admin" },
+          {
+            withCredentials: true,
+            headers: { "Content-Type": "application/json" },
+          }
+        )
+        .then((res) => {
+          toast.success(res.data.message);
+          setIsAuthenticated(true);
+          navigateTo("/");
+          setEmail("");
+          setPassword("");
+          setConfirmPassword("");
+        });
     } catch (error) {
-      toast.error(error?.response?.data?.message || "Login failed");
+      toast.error(error.response.data.message);
     }
   };
 
-  if (isAuthenticated) return <Navigate to="/" />;
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
 
   return (
-    <section className="container form-component">
-      <img src="/logo.png" alt="logo" className="logo" />
-      <h1 className="form-title">WELCOME TO MediServe</h1>
-      <p>Only Admins Are Allowed To Access These Resources!</p>
-      <form onSubmit={handleLogin}>
-        <input
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
-        <div style={{ justifyContent: "center", alignItems: "center" }}>
-          <button type="submit">Login</button>
-        </div>
-      </form>
-    </section>
+    <>
+      <section className="container form-component">
+        <img src="/logo.png" alt="logo" className="logo" />
+        <h1 className="form-title">WELCOME TO MediServe</h1>
+        <p>Only Admins Are Allowed To Access These Resources!</p>
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <div style={{ justifyContent: "center", alignItems: "center" }}>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 
