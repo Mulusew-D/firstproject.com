bf6a28fe024f80c030f3ee64e021bd474c4f124f
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-20 02:43:56 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,60 +1,43 @@
-import React, { useContext, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
-import AddNewDoctor from "./components/AddNewDoctor";
-import Messages from "./components/Messages";
-import Doctors from "./components/Doctors";
-import { Context } from "./main";
-import axios from "axios";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Sidebar from "./components/Sidebar";
-import AddNewAdmin from "./components/AddNewAdmin";
-import "./App.css";
 
-const App = () => {
-  const { isAuthenticated, setIsAuthenticated, admin, setAdmin } =
-    useContext(Context);
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import cookieParser from "cookie-parser";
+import {config} from "dotenv"
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+const app = express();
+config({path: "./config/config.env" });
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL_ONE || "https://mediserve-ruddy.vercel.app",
+      process.env.FRONTEND_URL_TWO || "https://mediserve-dashboard.vercel.app"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await axios.get(
-          "https://mediserve-backend.onrender.com/api/v1/user/admin/me",
-          {
-            withCredentials: true,
-          }
-        );
-        setIsAuthenticated(true);
-        setAdmin(response.data.user);
-      } catch (error) {
-        setIsAuthenticated(false);
-        setAdmin({});
-      }
-    };
-    fetchUser();
-  }, [isAuthenticated]);
 
-  return (
-    <Router>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/doctor/addnew" element={<AddNewDoctor />} />
-        <Route path="/admin/addnew" element={<AddNewAdmin />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/doctors" element={<Doctors />} />
-      </Routes>
-      <ToastContainer position="top-center" />
-    </Router>
-  );
-};
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-export default App;
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+app.use(errorMiddleware);
+export default app;
