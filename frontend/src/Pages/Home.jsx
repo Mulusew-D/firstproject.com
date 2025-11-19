34693c2bb4cd3740a8fd8ad83da4f2ed095b1eab
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-19 22:27:15 +0300

    initial commit

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <>
       <Hero
         title={
-          "Welcome to MediServe Medical Institute | Your Trusted Healthcare Provider"
+          "Welcome to MediServe Hospital Management & Appointment System"
         }
         imageUrl={"/hero.png"}
       />
