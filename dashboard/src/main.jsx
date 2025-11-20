054ecbb6f1691ffe1a8788612da501d22ed72ab9
Mulusew Desale <mulusewdesale@gmail.com>
2025-11-20 07:59:57 +0300

    initial commit

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -2,18 +2,15 @@ import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-export const Context = createContext({
-  isAuthenticated: false,
-  user: null
-});
+export const Context = createContext({ isAuthenticated: false });
 
 const AppWrapper = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
+  const [admin, setAdmin] = useState({});
 
   return (
     <Context.Provider
-      value={{ isAuthenticated, setIsAuthenticated, user, setUser }}
+      value={{ isAuthenticated, setIsAuthenticated, admin, setAdmin }}
     >
       <App />
     </Context.Provider>
